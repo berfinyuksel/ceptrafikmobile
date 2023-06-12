0a3e7b7d03fcf05e471493fe51fd585870aa1117
berfinyuksel <99557970+berfinyuksel@users.noreply.github.com>
2023-06-12 18:44:29 +0200

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:cep_trafik_package/cep_trafik_package.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Material App',
+      home: MainView(),
+    );
+  }
+}
